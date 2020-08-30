95b2125b0df607ccac3d07c0ce369baacc244a16
Ivani <katarzyna.stosio@gmail.com>
2020-08-30 18:26:10 +0200

    finished quiz with radio buttons

--- quiz-radiobuttons/radiobutton.js
+++ quiz-radiobuttons/radiobutton.js
@@ -1,5 +1,25 @@
 
 
+const answers = {
+    rome: 'Rzym',
+    platypus: 'Dziobak',
+    south: 'Na południu',
+    australia: 'Australia',
+    be: 'być'
+}
+
+document.body.addEventListener('change', (event) => {
+    console.log(event.target.value)
+    console.log(answers[event.target.id])
+    if(event.target.value === answers[event.target.id]){
+
+        alert('Odpowiedź poprawna!')
+        makeVisible()
+    } else {
+        alert('Odpowiedź niepoprawna :(')
+    }
+})
+
 function makeVisible(){
     document.querySelector('div.visible').className = 'invisible';
     document.querySelector('div.invisible.not-touched').className = 'visible';
