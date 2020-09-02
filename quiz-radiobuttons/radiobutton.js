5c054a9ec01549c39ea41958f0626b55bdf7c91e
Ivani <katarzyna.stosio@gmail.com>
2020-09-02 14:41:05 +0200

    added modal window instead alert

--- quiz-radiobuttons/radiobutton.js
+++ quiz-radiobuttons/radiobutton.js
@@ -1,4 +1,5 @@
 
+let pkt = 0;
 
 const answers = {
     rome: 'Rzym',
@@ -9,18 +10,62 @@ const answers = {
 }
 
 document.body.addEventListener('change', (event) => {
-    console.log(event.target.value)
-    console.log(answers[event.target.id])
+    
     if(event.target.value === answers[event.target.id]){
 
-        alert('Odpowiedź poprawna!')
+        //windowOnClick(event);
+        const positiveText = document.querySelector('.modal-text')
+        positiveText.textContent = 'Dobra odpowiedź!'
+        makeModalVisible();
+        pkt += 100;
         makeVisible()
+        
     } else {
-        alert('Odpowiedź niepoprawna :(')
+        
+        //windowOnClick(event);
+        const positiveText = document.querySelector('.modal-text')
+        positiveText.textContent = 'Spróbuj jeszcze raz.'
+        makeModalVisible();
     }
 })
 
+function showCongratz(){
+    const congratz = document.getElementsByClassName('congratz');
+    
+    congratz.textContent = 'Gratulacje, otrzymałeś ' + pkt + ' punktów!'
+    
+}
+
+function makeModalVisible(){
+    console.log('visible')
+    document.querySelector('.invisible.modal').className = 'visible modal'
+}
+
+function makeModalInvisible(){
+    console.log('invisible')
+    document.querySelector('.visible.modal').className = 'invisible modal'
+}
+
 function makeVisible(){
     document.querySelector('div.visible').className = 'invisible';
-    document.querySelector('div.invisible.not-touched').className = 'visible';
+    const firstInvisible = document.querySelector('div.invisible.not-touched');
+    console.log(firstInvisible)
+    if(firstInvisible.className.includes('end')){
+        showCongratz();
+    }
+    firstInvisible.className = 'visible';
 }
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close-button");
+
+
+
+function windowOnClick(event) {
+    console.log(event.target.className)
+    if (event.target.className === 'close-button' || event.target.className === 'visible modal') {
+        makeModalInvisible();
+    }
+}
+
+
+window.addEventListener("click", windowOnClick);
